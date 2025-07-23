bb7251e02cfb285001935811f3ae3bd1bd90b67f
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-07-23 17:46:34 -0300

    fix
    arrumando o import

--- src/routes/Cadastrar_discos_excel.jsx
+++ src/routes/Cadastrar_discos_excel.jsx
@@ -1,6 +1,6 @@
 import Swal from "sweetalert2";
 import Footer from "../components/Footer";
-import Form_add_excel from "../components/form_add_excel";
+import Form_add_excel from "../components/Form_add_excel";
 import Header from "../components/Header";
 import Cadastrar_discos_style from "../styles/Cadastrar_discos_excel_style";
 
@@ -14,7 +14,7 @@ const Cadastrar_discos_excel = () => {
             <main>
                 <section className="section-form-add-excel">
                     <div className="form-esquerda">
-                        <h1>Cadastre com a sua tabela</h1>
+                        <h1>Cadastre com a sua tabela!</h1>
                     </div>
 
                     <Form_add_excel/>
