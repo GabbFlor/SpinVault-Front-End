77c843b251a4b2b1a690e0a88c1961c0e481cff4
mrBicarato <matheusbicarato@gmail.com>
2025-08-06 16:36:48 -0300

    fix
    rotas para o contato

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import Logo from "../assets/logo_footer.webp"
 import { BsInstagram, BsFacebook, BsTwitterX } from "react-icons/bs"
 import Footer_style from "../styles/Footer_style";
+import { Link, } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -19,15 +20,23 @@ const Footer = () => {
                     Seus dados pessoais são coletados e protegidos de forma segura, em total conformidade com a Lei Geral de Proteção de Dados (LGPD). <br />
                     Ao utilizar nosso site, você concorda com nossa Política de Privacidade e com os Termos de Uso que regem nossa plataforma.</p>
             </div>
+            <div className="contatos">
+                <div className="pagina-contato">
+                    <nav>
+                        <Link to={"/contato"}>entre em contato</Link>
+                    </nav>
+                </div>
+                <div className="div-redes-sociais">
+                    <p>Siga:</p>
 
-            <div className="div-redes-sociais">
-                <p>Siga:</p>
-
-                <nav>
-                    <a href="https://www.instagram.com/spinvault_oficial/" target="blank_"><BsInstagram /></a>
-                    <a href="https://www.facebook.com/profile.php?id=61577318084363" target="blank_"><BsFacebook /></a>
-                </nav>
+                    <nav>
+                        <a href="https://www.instagram.com/spinvault_oficial/" target="blank_"><BsInstagram /></a>
+                        <a href="https://www.facebook.com/profile.php?id=61577318084363" target="blank_"><BsFacebook /></a>
+                    </nav>
+                </div>
             </div>
+
+
         </footer>
     )
 }
--- src/styles/Footer_style.jsx
+++ src/styles/Footer_style.jsx
@@ -36,7 +36,31 @@ const Footer_style = createGlobalStyle`
         text-align: center;
         font-size: 1vw;
     }
+    .contatos {
+        display: flex;
+        flex-direction: row;
 
+    }
+    .pagina-contato {
+        display: inline-flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 5px;
+        padding: 5px 0;
+    }
+    .pagina-contato a {
+        display: inline-flex;
+        align-items: center;
+        font-size: 1.25vw;
+        color: black;
+    }
+    .pagina-contato nav {
+        display: inline-flex;
+        align-items: center;
+        font-size: 1.45vw;
+        gap: 5px;
+        cursor: pointer;
+    }
     .div-redes-sociais {
         display: inline-flex;
         align-items: center;
@@ -64,6 +88,9 @@ const Footer_style = createGlobalStyle`
         .div-redes-sociais a {
             font-size: 2.25vw;
         }
+        .pagina-contato a {
+            font-size: 2.25vw;
+        }
         .div-text p {
         text-align: center;
         font-size: 1.50vw;
@@ -72,6 +99,9 @@ const Footer_style = createGlobalStyle`
 
     @media (max-width: 500px) {
         .div-redes-sociais a {
+            font-size: 4vw;
+        }
+        .pagina-contato a {
             font-size: 4vw;
         }
         .div-text p {
