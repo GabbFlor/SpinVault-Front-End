a06858d3e72df211b233f130ba1d7eafba009ac9
mrBicarato <matheusbicarato@gmail.com>
2025-09-10 12:45:46 -0300

    fix

--- src/components/Relacao_internacional.jsx
+++ src/components/Relacao_internacional.jsx
@@ -284,7 +284,8 @@ const Relacao_internacional = () => {
                                             {disco.observacoes && disco.observacoes.length > 10
                                                 ? disco.observacoes.slice(0, 10) + "..."
                                                 : disco.observacoes}
-                                        </td>                                        <td><Link to={`/editar-disco/${discoFiltrado.id}`} className="btn-ver-mais">Editar</Link></td>
+                                        </td>
+                                        <td><Link to={`/editar-disco/${discoFiltrado.id}`} className="btn-ver-mais">Editar</Link></td>
                                     </tr>
                                 ))
                             ) : (
